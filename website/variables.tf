6a5daad926d3c719a388a5c23d924a91abb7737f
Arpakathy <kathyghomo@gmail.com>
2024-03-27 11:45:55 +0100

    Upload main.tf variables.tf and Update config.yml

---
+++ website/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "bucket_name" {
+    type = string
+    description = "The name of the your bucket"
+    default = "bucketParlai"   
+}
+
+variable "cp-path" {
+  type = string
+  description = "PATH where the website files are located"
+  default = "build"
+}
+
+variable "file-key" {
+  type    = string
+  default = "index.html"
+}
+
